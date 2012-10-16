ba335d2181ae499b691459d5a863c91e79e961b8
Giscard Biamby <giscardo@yahoo.com>
2012-10-16 17:18:24 -0400

    Added Admin UI to select theme; bundled some themes (default =github, if not selected)

--- Handlers/SyntaxHighlighterPartHandler.cs
+++ Handlers/SyntaxHighlighterPartHandler.cs
@@ -1,18 +1,18 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using JetBrains.Annotations;
-//using Orchard.ContentManagement.Handlers;
-//using Orchard.Data;
-//using Orchard.Blogs.Models; 
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.Annotations;
+using Orchard.ContentManagement.Handlers;
+using Orchard.Data;
+using Orchard.Blogs.Models;
+using TheMonarch.SyntaxHighlighter.Models;
 
-//namespace TheMonarch.SyntaxHighlighter.Handlers {
-//    [UsedImplicitly]
-//    public class SyntaxHighlighterPartHandler : ContentHandler {
-//        public ScheduledActionTaskPartHandler(IRepository<ScheduledActionTaskRecord> repository)
-//        {
-//            Filters.Add(new ActivatingFilter<SyntaxHighlighterPart>("ScheduledActionTask"));
-//        }
-//    }
-//}
+namespace TheMonarch.SyntaxHighlighter.Handlers {
+    [UsedImplicitly]
+    public class SyntaxHighlighterPartHandler : ContentHandler {
+        public SyntaxHighlighterPartHandler() {
+            Filters.Add(new ActivatingFilter<SyntaxHighlighterPart>("BlogPost"));
+        }
+    }
+}
--- ResourceManifest.cs
+++ ResourceManifest.cs
@@ -47,12 +47,21 @@ namespace TheMonarch.SyntaxHighlighter {
             var manifest = builder.Add();
 
             //Styles
-            manifest.DefineStyle("TheMonarch.prettify").SetUrl(ModuleStyle("pretty/prettify.css"));
-            manifest.DefineStyle("TheMonarch.prettify-tubster").SetUrl(ModuleStyle("pretty/prettify-tomorrow-night.css"));
+            manifest.DefineStyle("TheMonarch.prettify-prettify").SetUrl(ModuleStyle("pretty/prettify.css")).SetVersion("1.0");
+            manifest.DefineStyle("TheMonarch.prettify-desert").SetUrl(ModuleStyle("pretty/desert.css")).SetVersion("1.0");
+            manifest.DefineStyle("TheMonarch.prettify-github").SetUrl(ModuleStyle("pretty/github.css")).SetVersion("1.0");
+            manifest.DefineStyle("TheMonarch.prettify-hemisu-dark").SetUrl(ModuleStyle("pretty/hemisu-dark.css")).SetVersion("1.0");
+            manifest.DefineStyle("TheMonarch.prettify-hemisu-light").SetUrl(ModuleStyle("pretty/hemisu-light.css")).SetVersion("1.0");
+            manifest.DefineStyle("TheMonarch.prettify-son-of-obsidian").SetUrl(ModuleStyle("pretty/son-of-obsidian.css")).SetVersion("1.0");
+            manifest.DefineStyle("TheMonarch.prettify-sunburst").SetUrl(ModuleStyle("pretty/sunburst.css")).SetVersion("1.0");
+            manifest.DefineStyle("TheMonarch.prettify-vibrant-ink").SetUrl(ModuleStyle("pretty/vibrant-ink.css")).SetVersion("1.0");
+            manifest.DefineStyle("TheMonarch.prettify-tomorrow").SetUrl(ModuleStyle("pretty/tomorrow.css")).SetVersion("1.0");
 
 
             // Scripts
             manifest.DefineScript("TheMonarch.prettify").SetUrl(ModuleScript("pretty/prettify.js")).SetDependencies("jQuery");
+            manifest.DefineScript("TheMonarch.prettify-css").SetUrl(ModuleScript("pretty/prettify-css.js")).SetDependencies("TheMonarch.prettify");
+            manifest.DefineScript("TheMonarch.prettify-sql").SetUrl(ModuleScript("pretty/prettify-sql.js")).SetDependencies("TheMonarch.prettify");
         }
     }
 }
